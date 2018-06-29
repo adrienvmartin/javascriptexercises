11fb12359e000467b7125dfc8bbdfe8665f67cfd
Adrien Martin <adrien.v.martin@gmail.com>
2018-06-29 15:09:40 -0600

    Finished exercise 5

---
+++ Ex05/index5.js
@@ -0,0 +1,11 @@
+var firstName = "Adrien";
+var lastName = "Martin";
+var age = 28;
+var dateOfBirth = "02/15/1990";
+var address = "123 Main Street";
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(dateOfBirth);
+console.log(address);
