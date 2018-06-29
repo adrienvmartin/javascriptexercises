a89a7f579ca4e1bcc6abec9c04566cc801eb5e79
Adrien Martin <adrien.v.martin@gmail.com>
2018-06-29 15:01:35 -0600

    did exercise 2

---
+++ Ex02/index2.js
@@ -0,0 +1,11 @@
+var firstName;
+var lastName;
+var age;
+var dateOfBirth;
+var address;
+
+firstName = 'Adrien';
+lastName = 'Martin';
+age = 28;
+dateOfBirth = '02/15/1990';
+address = "RR 8 Site 812 Comp 4, Saskatoon, Sk";
