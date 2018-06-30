d47e475e8e79325121c1467389c1a6e67d44fa10
Adrien Martin <adrien.v.martin@gmail.com>
2018-06-29 19:16:43 -0600

    Finished exercise 21

---
+++ Ex21/index21.js
@@ -0,0 +1,12 @@
+let age = 28;
+let phone = 1234567;
+let streetName = "Research Drive"
+let streetNumber = 111;
+
+let template1 = `I'm ${age} years old`;
+let template2 = `My phone number is ${phone}`;
+let template3 = `I live on ${streetNumber} ${streetName}`;
+
+console.log(template1);
+console.log(template2);
+console.log(template3);
