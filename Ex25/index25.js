6f6e687a8c6210bc9fec4042df0e49e54a5ab0a3
Adrien Martin <adrien.v.martin@gmail.com>
2018-06-29 19:36:46 -0600

    Finished exercises 25 and 26

---
+++ Ex25/index25.js
@@ -0,0 +1,20 @@
+let superHeroName = "Batman";
+let name = "Bruce Wayne";
+let fly = false;
+let hasBatmobile = true;
+let life = 75;
+let freeTime = null;
+
+console.log("Superhero Name: " + superHeroName);
+console.log("Name: " + name);
+console.log("Can fly: " + fly);
+console.log("Has a Batmobile: " + hasBatmobile);
+console.log("Life percentage left: " + life);
+console.log("Has free time: " + freeTime);
+
+console.log(typeof superHeroName);
+console.log(typeof name);
+console.log(typeof fly);
+console.log(typeof hasBatmobile);
+console.log(typeof life);
+console.log(typeof freeTime);
---
+++ Ex26/index26.js
@@ -0,0 +1,15 @@
+let city = "Winnipeg";
+let province = "Manitoba";
+let population = 750000;
+let warm = false;
+let rapidTransit = null;
+let scenery = undefined;
+
+console.log("City: " + city + "\n" + "Province: " + province + "\n" + "Population: " + population + "\n" + "Warm weather: " + warm + "\n" + "Name of rail transit system: " + rapidTransit + "\n" + "Description of surrounding scenery: " + scenery);
+
+/* console.log("City: " + city);
+console.log("Province: " + province);
+console.log("Population: " + population);
+console.log("Warm weather: " + warm);
+console.log("Name of rail transit system: " + rapidTransit);
+console.log("Description of surrounding scenery: " + scenery); */
