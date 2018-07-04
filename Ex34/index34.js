5b6aca12efa63fa627cefa058ee8d70cae72d139
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-04 15:23:52 -0600

    Finished exercise 34

---
+++ Ex34/index34.js
@@ -0,0 +1,11 @@
+let income = 1000;
+let revenue = 600;
+let taxes = 500;
+
+let incomeObjective = 800;
+let taxesObjective = 400;
+let revenueObjective = 600;
+
+console.log(`Income objective: `, income >= incomeObjective);
+console.log(`Taxes objective: `, taxes < taxesObjective);
+console.log(`Bonus pay: `, revenue == revenueObjective);
