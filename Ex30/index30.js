50bbb2898ae20be4e21914f6139664d3c8ade503
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-04 14:33:44 -0600

    Finished exercise 30

---
+++ Ex30/index30.js
@@ -0,0 +1,6 @@
+let sideLength = 5;
+let area = Math.pow(sideLength, 2);
+
+let message = `The area of a square can be calculated by multiplying the length and width together, or squaring the side length (since they're all the same on a square). The length of our side is ${sideLength}, therefore the area is ${sideLength} squared, or ${area}`;
+
+console.log(message);
