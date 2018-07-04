8db95a35184471ed7a4313b29f543060a68291f0
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-04 15:31:20 -0600

    Finished exercises 36 and 37

---
+++ Ex37/index37.js
@@ -0,0 +1,7 @@
+let average = 6;
+let studentAverage = 4;
+
+let points = average - studentAverage;
+
+console.log(`This student has a greater of equal average required to pass:`, studentAverage >= average);
+console.log(`This student is missing ${points} points to pass`);
