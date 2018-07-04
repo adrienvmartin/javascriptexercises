b3b92bc2bf25a0f51282267aef43604c43ddb2ed
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-04 15:17:43 -0600

    Finished exercise 33

---
+++ Ex33/index33.js
@@ -0,0 +1,17 @@
+let userCount = 100;
+userCount += 5;
+
+console.log(`User count: ${userCount}`);
+
+userCount -= 3;
+
+console.log(`User count: ${userCount}`);
+
+userCount *= 2;
+
+console.log(`User count after doubling the company: ${userCount}`);
+
+menCount = userCount/2;
+womenCount = userCount/2;
+
+console.log(`User count: Men - ${menCount}, Women - ${womenCount}`);
