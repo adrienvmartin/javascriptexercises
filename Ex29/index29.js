85835ade2bcee3569f495f24fd34ee62913aaa76
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-04 14:27:54 -0600

    Finished exercise 29

---
+++ Ex29/index29.js
@@ -0,0 +1,5 @@
+let sideLength = 10;
+
+let message = `The perimeter of a square can be calculated by the formula 4S, where "S" equals the side length. The side length of our square is ${sideLength}. Four times our side length is equal to ${sideLength * 4}`;
+
+console.log(message);
