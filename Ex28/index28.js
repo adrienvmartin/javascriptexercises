a2f6f653d8d62e2ebe12fabacce14fc80fa5ad3d
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-04 14:25:32 -0600

    Finished exercise 28

---
+++ Ex28/index28.js
@@ -0,0 +1,9 @@
+let varNumber = 9;
+
+let lineOne = `${varNumber} times 1 equals ${varNumber * 1}`;
+let lineTwo = `${varNumber} times 2 equals ${varNumber * 2}`;
+let lineThree = `${varNumber} times 3 equals ${varNumber *3}`;
+
+console.log(lineOne);
+console.log(lineTwo);
+console.log(lineThree);
