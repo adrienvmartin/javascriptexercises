79b97f5152005505f1e8b47492f6f3e99c838eea
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-04 14:36:30 -0600

    Finished exercise 31

---
+++ Ex31/index31.js
@@ -0,0 +1,7 @@
+let firstSide = 10;
+let secondSide = 20;
+let thirdSide = 5;
+
+let perimeter = firstSide + secondSide + thirdSide;
+
+console.log(`The perimeter of a triangle can be calculated by adding all three sides. In our case, the sides are 10, 20, and 5 in length, and thus the perimeter is ${perimeter}`);
