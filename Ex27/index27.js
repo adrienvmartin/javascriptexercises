e156edb61daceba0110e94f7a03eb7299df54474
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-04 14:20:56 -0600

    Finished exercise 27

---
+++ Ex27/index27.js
@@ -0,0 +1,12 @@
+let firstNumber;
+let secondNumber;
+
+firstNumber = 24;
+secondNumber = 44;
+
+let addOutput = `I add ${firstNumber} and ${secondNumber} and get a result of ${firstNumber + secondNumber}`;
+let subOutput = `I subtract ${firstNumber} from ${secondNumber} and get a result of ${firstNumber - secondNumber}`;
+let multOutput = `I multiply ${firstNumber} by ${secondNumber} and get a result of ${firstNumber * secondNumber}`;
+let divOutput = `I divide ${firstNumber} by ${secondNumber} and get a result of ${firstNumber / secondNumber}`;
+
+console.log(addOutput + "\n" + subOutput + "\n" + multOutput + "\n" + divOutput);
