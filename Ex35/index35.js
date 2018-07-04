827544b28d9b0c55fee305a1977911882127a845
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-04 15:26:46 -0600

    Finished exercise 35

---
+++ Ex35/index35.js
@@ -0,0 +1,5 @@
+let clientId = 143245;
+let inputClientId = 143245;
+
+console.log(`The client ID is correct by value: `, clientId == 143245);
+console.log(`The client ID is correct by value and type: `, clientId == inputClientId);
