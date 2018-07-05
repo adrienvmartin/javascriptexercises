1301fef6b27494fff351353bd0afaaa14caf3923
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-05 11:53:23 -0600

    Finished exercise 124

---
+++ Ex124/index124.js
@@ -0,0 +1,15 @@
+let movies = [
+    "Star Wars",
+    "Interstellar",
+    "It",
+    "Sicario",
+    "A Clockwork Orange",
+    "Back to the Future",
+    "Indiana Jones",
+    "Dunkirk",
+    "2001: A Space Odyssey",
+    "Lord of the Rings"
+];
+
+console.log(movies[0]);
+console.log(movies[movies.length -1 ]);
