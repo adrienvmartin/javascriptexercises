d9640347c3075a7c93cabe5645d2bb33da9ac469
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-04 23:12:38 -0600

    Updated exercise 107

--- Ex107/index107.js
+++ Ex107/index107.js
@@ -1,13 +1,23 @@
 function biggerNumber(firstNumber, secondNumber) {
-    if (firstNumber > secondNumber) {
-        console.log(`Number ${firstNumber} is bigger than ${secondNumber}`);
-    }
-    else if (secondNumber > firstNumber) {
-        console.log(`Number ${secondNumber} is bigger than ${firstNumber}`);
-    }
+    if (typeof firstNumber != 'number' && typeof secondNumber != 'number') {
+        console.log("These are not numbers!"); }
 
-    else {
-        console.log(`Both numbers are ${firstNumber}!`);
+    else if (typeof firstNumber != 'number' || typeof secondNumber != 'number') {
+            console.log("One of these is not a number!");
+        }
+
+    else if (typeof firstNumber == 'number' && typeof secondNumber == 'number') {
+    
+        if (firstNumber > secondNumber) {
+            console.log(`Number ${firstNumber} is bigger than ${secondNumber}`);
+        }
+        else if (secondNumber > firstNumber) {
+            console.log(`Number ${secondNumber} is bigger than ${firstNumber}`);
+        }
+
+        else if (firstNumber == secondNumber) {
+            console.log(`Both numbers are ${firstNumber}`);
+        }
     }
 
 }
@@ -15,3 +25,6 @@ function biggerNumber(firstNumber, secondNumber) {
 biggerNumber(3,5);
 biggerNumber(6,3);
 biggerNumber(2,2);
+biggerNumber("two","three");
+biggerNumber("yellow","green");
+biggerNumber("blue",5);
