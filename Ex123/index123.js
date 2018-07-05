301b257ce46fa7928427cc58e42f11e5638400b0
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-05 11:48:45 -0600

    Finished exercise 123

---
+++ Ex123/index123.js
@@ -0,0 +1,13 @@
+let students = [
+    "John",
+    "Billy",
+    "Janey",
+    "Sarah",
+    "Kyle",
+    "Bobby",
+    "Alicia"
+];
+
+console.log(students[1].toUpperCase());
+console.log(students[5].toUpperCase());
+console.log(`The students array has ` + students.length + ` items`);
