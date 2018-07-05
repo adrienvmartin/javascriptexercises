c64f8505ec1f5e817c86badf3bda50a59a203867
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-04 20:23:16 -0600

    Finished exercise 101

---
+++ Ex101/index101.js
@@ -0,0 +1,17 @@
+let name = "Santa Claus";
+let age = 150;
+let phone = "555-1234";
+let street = "101 Frozen Lane, North Pole";
+let postal = "H0H 0H0";
+let married = true;
+
+function showUser() {
+    console.log(`Name: ${name}`);
+    console.log(`Age: ${age}`);
+    console.log(`Phone Number: ${phone}`);
+    console.log(`Address: ${street}`);
+    console.log(`Postal code: ${postal}`);
+    console.log(`Married: ${married}`);
+}
+
+showUser();
