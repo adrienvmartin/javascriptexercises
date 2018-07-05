3c744a6341b501e60b4a61f1d68136ee1f031767
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-05 11:42:09 -0600

    Finished exercise 121

---
+++ Ex121/index121.js
@@ -0,0 +1,17 @@
+let games = [
+    "Grand Theft Auto V",
+    "The Last Of Us Remastered",
+    "Persona 5",
+    "Metal Gear Solid V",
+    "Uncharted 4",
+    "Journey",
+    "Bloodborne",
+    "The Witcher 3",
+    "Diablo III"
+];
+
+games[0] = "Fifa 18";
+games[3] = "Fifa 17";
+games[2] = "Fifa 16";
+
+console.log(games);
