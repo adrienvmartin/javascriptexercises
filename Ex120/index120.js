4aa973ea800934bfd391351b2cc7a452bac9cf1e
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-05 11:37:36 -0600

    Finished exercise 120

---
+++ Ex120/index120.js
@@ -0,0 +1,19 @@
+let students = [];
+
+students[0] = 'Marta';
+students[1] = 'Pedro';
+students[2] = 'Jackson';
+students[3] = 'Danielle';
+students[4] = 'Homer';
+students[5] = 'Bart';
+
+function printStudents() {
+    console.log(`Index 0: ${students[0]}`);
+    console.log(`Index 1: ${students[1]}`);
+    console.log(`Index 2: ${students[2]}`);
+    console.log(`Index 3: ${students[3]}`);
+    console.log(`Index 4: ${students[4]}`);
+    console.log(`Index 5: ${students[5]}`);
+}
+
+printStudents();
