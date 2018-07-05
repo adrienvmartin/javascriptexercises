18a62f790fc1043900c455c2cf9d48738216f238
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-05 13:52:01 -0600

    Finished exercise 145

---
+++ Ex145/index145.js
@@ -0,0 +1,12 @@
+let user = {
+    username: 'batman',
+    password: 'Alfred1960ROCKS!'
+};
+
+if (user.username == 'batman' && user.password == 'Alfred1960KPO!') {
+    console.log("Welcome back, Batman!");
+}
+
+else {
+    console.log("Sorry, you can't enter the Batcave, try again.");
+}
