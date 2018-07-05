70993c9f83d50f349363d9d01a7b2b4c0b216f8c
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-05 09:31:50 -0600

    Finished exercise 110

---
+++ Ex110/index110.js
@@ -0,0 +1,7 @@
+function average(num1, num2, num3, num4, num5) {
+    let sumTotal = (num1 + num2 + num3 + num4 + num5);
+    let averageNumber = (sumTotal / 5);
+    console.log(`The average is ${averageNumber}`);
+}
+
+average(2,4,10,20,30);
