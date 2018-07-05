fa57cc89063e53e170c128dd960b6103b78124fe
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-05 11:44:51 -0600

    Finished exercise 122

---
+++ Ex122/index122.js
@@ -0,0 +1,19 @@
+let ingredients = [
+    "pork",
+    "tomato",
+    "chicken",
+    "lettuce",
+    "beef",
+    "carrots",
+    "cucumber"
+];
+
+let vegetarian = [
+    ingredients[1],
+    ingredients[3],
+    ingredients[5],
+    ingredients[6]
+];
+
+console.log(`Full ingredients: ${ingredients}`);
+console.log(`Vegetarian ingredients: ${vegetarian}`);
