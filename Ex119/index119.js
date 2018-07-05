a4c7ddff39bcb6f948b50aa0bf7e2f7d31acc030
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-05 14:36:54 -0600

    Figured out exercise 119

--- Ex119/index119.js
+++ Ex119/index119.js
@@ -1,13 +1,9 @@
-const data = [
-    42, 
-    true, 
-    function() { console.log('The meaning of life is: ') }
-];
+function meaningOfLife() {
+    return `The meaning of life is: `
+}
 
-const message = data[2];
-const number = data[0];
+const data = [42, true, meaningOfLife()];
 
 if (data[1] == true) {
-    console.log(message);
-    console.log(number);
+    console.log(data[2] + data[0]);
 }
