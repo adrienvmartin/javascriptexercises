9c195f121a31b4b8760eccff0be47a4199c22985
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-05 18:35:50 -0600

    Finished exercise 151

---
+++ Ex151/index151.js
@@ -0,0 +1,11 @@
+let groot = {
+    greet: function() {
+        console.log("I am Groot!");
+    },
+    speak: function() {
+        console.log("I am Groot!");
+    },
+};
+
+groot.speak();
+groot.greet();
