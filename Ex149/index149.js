6120d167396ffd1edbe5497afbe56cc3f43a70c8
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-05 18:11:52 -0600

    Finished exercise 149

---
+++ Ex149/index149.js
@@ -0,0 +1,16 @@
+let user = {
+    name: "Adrien",
+    age: 28,
+    city: {
+        name: "Saskatoon",
+        location: "Saskatchewan",
+        climate: "cold"
+    },
+
+    hobbies: ["music", "movies", "coding"],
+};
+
+user.program = "comIT";
+user.married = false;
+
+console.log(user);
