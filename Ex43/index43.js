4c1f3682cb675e8c67267def727a0c836d2a6217
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-06 11:48:52 -0600

    Finished exercise 43

---
+++ Ex43/index43.js
@@ -0,0 +1,9 @@
+let text = "game of thrones";
+let firstChar = text.charAt(0);
+let secondChar = text.charAt(5);
+let thirdChar = text.charAt(8);
+
+let result = firstChar + secondChar + thirdChar;
+let message = `The final result is ${result.toUpperCase()}`;
+
+console.log(message);
