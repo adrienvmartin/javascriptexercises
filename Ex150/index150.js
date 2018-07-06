df5efedba7c058998a6621087f08449380f99024
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-05 18:30:06 -0600

    Finished exercise 150

---
+++ Ex150/index150.js
@@ -0,0 +1,10 @@
+function answer() {
+    return 42;
+}
+
+let deepThought = {
+    name: "Deep Thought",
+    answerToTheUltimateQuestionOfLife: answer(),
+};
+
+console.log(deepThought.answerToTheUltimateQuestionOfLife);
