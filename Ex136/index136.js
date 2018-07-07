de8e75f4678d0412165e90b5544a19513a31002f
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-06 19:25:19 -0600

    Finished exercise 136

---
+++ Ex136/index136.js
@@ -0,0 +1,29 @@
+let mutants = [
+    "Professor X",
+"Cyclops",
+"Iceman",
+"Angel",
+"Magneto",
+"Beast",
+"Phoenix",
+"Logan",
+"Gambit",
+];
+
+let newMutants = [];
+
+mutants.forEach(function(mutant) {
+    if (mutant == "Iceman") {
+        newMutants.push(mutant);
+    }
+    else if (mutant == "Logan") {
+        newMutants.push(mutant);
+    }
+
+    else if (mutant == "Phoenix") {
+        newMutants.push(mutant);
+    }
+});
+
+newMutants.join();
+console.log(newMutants.toString());
