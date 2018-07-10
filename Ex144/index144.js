5315cc3e8f0936b0da7598af4d7c13e9abd9dd15
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-09 18:29:44 -0600

    Updated exercise 144

--- Ex144/index144.js
+++ Ex144/index144.js
@@ -5,5 +5,12 @@ const user = {
 
 console.log(user);
 
-console.log(user.username.toUpperCase());
+function capitalize(string) {
+    let newstring = string.charAt(0).toUpperCase() + string.slice(1);
+    return newstring;
+}
+
+// Alt version: console.log(user.username.slice(0,1).toUpperCase() + user.username.slice(1));
+
+console.log(capitalize(user.username));
 console.log(user.password.replace(/1960/, " "));
