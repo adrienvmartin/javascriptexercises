2ec1a44f6a365f7d2a24b05649659aa7379420fd
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-11 22:25:50 -0600

    Finished the longest word in a string exercise

--- RandomExercises/random02.js
+++ RandomExercises/random02.js
@@ -1,11 +1,26 @@
+function checkLongestWord(string) {
+    let strSplit = string.split(" ");
+    let counter = 0;
+    let longestWord = null;
+    for (i = 0; i < strSplit.length; i++) {
+        if (strSplit[i].length > counter) {
+            longestWord = strSplit[i];
+        }
+    }
+    console.log(`The longest word is: ${longestWord}`);
+}
+
+checkLongestWord("This is a test of the string length checker");
+
+/* Old non-working version
 function longestWord(string) {
     let characters = Array.from(string);
     let newString = [];
     let counter = 0;
     function validate(strValue) {
-        let objRegExp  = /^[a-z]+$/;
+        let objRegExp = /^[a-z]+$/;
         return objRegExp.test(strValue);
-      }
+    }
     function wordCounter() {
         for (let i = 0; i < characters.length; i++) {
             if (validate(string) == true) {
@@ -18,6 +33,8 @@ function longestWord(string) {
         }
     }
 
+    console.log(characters);
+
 }
 
-longestWord("Hey this is a string");
+longestWord("Hey this is a string"); */
