391c0365b9c4a35d937eb4c31b6fcae3ec305830
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-11 19:33:55 -0600

    Working on random Javascript exercises

--- RandomExercises/random02.js
+++ RandomExercises/random02.js
@@ -1,3 +1,24 @@
 function longestWord(string) {
-    
+    let characters = Array.from(string);
+    let newString = [];
+    let counter = 0;
+    function validate(strValue) {
+        let objRegExp  = /^[a-z]+$/;
+        return objRegExp.test(strValue);
+      }
+    function wordCounter() {
+        for (let i = 0; i < characters.length; i++) {
+            if (validate == true) {
+                newString[counter].push(characters[i]);
+            }
+            else {
+                counter++;
+                wordCounter();
+            }
+        }
+    }
+    console.log(letters);
+
 }
+
+longestWord("Hey this is a string");
